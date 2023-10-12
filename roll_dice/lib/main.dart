d3aa1c88d5addc5af57b9475ef3a8b6b29026072
nothing-mann <i.sudeep.subedi@gmail.com>
2023-10-12 16:26:02 +0545

    Some styling using Containers (inside Scaffold) and its attributes (decoration: BoxDecoration) also styled the texts.

--- roll_dice/lib/main.dart
+++ roll_dice/lib/main.dart
@@ -2,10 +2,24 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World'),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Color(0xffff7676), Color(0x99ff7676)],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello World',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 28.0),
+            ),
+          ),
         ),
       ),
     ),
